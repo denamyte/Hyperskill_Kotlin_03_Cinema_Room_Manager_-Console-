396e6805efdf939bcadc43e1d539dc00834115ac
denys <denamyte@gmail.com>
2023-03-22 22:32:51 +0200

    2023-03-22: Stage 1/5

--- Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
+++ Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
@@ -1,5 +1,18 @@
 package cinema
 
+const val ROW_NUMBER = 7
+const val SEAT_NUMBER = 8
+const val HEADERS = "Cinema:\n  1 2 3 4 5 6 7 8"
+const val ROW_TEMPLATE = "%d %s %s %s %s %s %s %s %s"
+val cinema = Array(ROW_NUMBER) { Array(SEAT_NUMBER) { "S" } }
+
 fun main() {
-    // write your code here
+    printCinema()
+}
+
+fun printCinema() {
+    println(HEADERS)
+    cinema.forEachIndexed { i, row ->
+        println(ROW_TEMPLATE.format(i + 1, *row))
+    }
 }
