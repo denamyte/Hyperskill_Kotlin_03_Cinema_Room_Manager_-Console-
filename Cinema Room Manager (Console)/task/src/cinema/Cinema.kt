f37ef6291c96979f8c4796653209130997224913
denys <denamyte@gmail.com>
2023-03-26 23:07:49 +0300

    2023-03-26: Stage 4/5

--- Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
+++ Cinema Room Manager (Console)/task/src/cinema/Cinema.kt
@@ -3,8 +3,9 @@ package cinema
 
 const val ROWS_PROMPT = "Enter the number of rows:"
 const val SEATS_PROMPT = "Enter the number of seats in each row:"
-const val BUSY_ROW_PROMPT = "\nEnter a row number:"
-const val BUSY_SEAT_PROMPT = "Enter a seat number in that row:"
+const val MENU = "\n1. Show the seats\n2. Buy a ticket\n0. Exit"
+const val OCCUPY_ROW_PROMPT = "\nEnter a row number:"
+const val OCCUPY_SEAT_PROMPT = "Enter a seat number in that row:"
 const val FRONT_PRICE = 10
 const val BACK_PRICE = 8
 const val SMALL_AUDITORIUM_LIMIT = 60
@@ -15,14 +16,24 @@ fun main() {
     val rowNumber = enterNumber(ROWS_PROMPT)
     val seatNumber = enterNumber(SEATS_PROMPT)
     val aud = Array(rowNumber) { Array(seatNumber) { VACANT } }
-    printAuditorium(aud)
+
+    while (true) {
+        println(MENU)
+        when (readln().toInt()) {
+            1 -> printAuditorium(aud)
+            2 -> buyTicket(aud)
+            0 -> break
+        }
+    }
+}
+
+fun buyTicket(aud: Array<Array<Char>>) {
     val busy = Pair(
-        enterNumber(BUSY_ROW_PROMPT) - 1,
-        enterNumber(BUSY_SEAT_PROMPT) - 1
+        enterNumber(OCCUPY_ROW_PROMPT) - 1,
+        enterNumber(OCCUPY_SEAT_PROMPT) - 1
     )
     setBusySeat(busy, aud)
-    printTicketPrice(getTicketPrice(busy.first, rowNumber, seatNumber))
-    printAuditorium(aud)
+    printTicketPrice(getTicketPrice(busy.first, aud.size, aud[0].size))
 }
 
 fun enterNumber(prompt: String): Int {
@@ -42,10 +53,10 @@ fun getTicketPrice(row: Int, rowNumber: Int, seatNumber: Int): Int {
     ) FRONT_PRICE else BACK_PRICE
 }
 
-fun printTicketPrice(price: Int) {
-    println("\nTicket price: \$$price")
-}
-
 fun setBusySeat(busy: Pair<Int, Int>, aud: Array<Array<Char>>) {
     aud[busy.first][busy.second] = BUSY
+}
+
+fun printTicketPrice(price: Int) {
+    println("Ticket price: \$$price")
 }
